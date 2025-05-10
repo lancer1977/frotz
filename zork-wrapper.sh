083de57b69d44fade21eadad1389e946885c9392
Christopher Richmond <crichmond@enclarapharmacia.com>
2025-05-10 13:14:18 -0700

    log

--- zork-wrapper.sh
+++ zork-wrapper.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-export TERM=vt100
-exec /usr/games/frotz "/games/$GAME_FILE"
+export TERM=vt100 
+
+#!/bin/bash
+
+LOGFILE=/games/zork.log
+GAMEFILE="/games/${GAME_FILE}"
+
+# Ensure log file is writable
+touch "$LOGFILE"
+chmod 666 "$LOGFILE"
+
+# Log execution
+echo "Starting Frotz with $GAMEFILE" >> "$LOGFILE"
+/usr/games/frotz  "$GAMEFILE" >> "$LOGFILE" 2>&1
